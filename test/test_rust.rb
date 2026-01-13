4fb298745a514d9df5ab182763b84f19678ce87b
Luke Francl <look@github.com>
2026-01-12 16:22:52 -0800

    add tests to exercise Rust code

---
+++ test/test_rust.rb
@@ -0,0 +1,46 @@
+require_relative "./helper"
+
+# This test verifies that the Rust bindings work.
+class TestRust < Minitest::Test
+  def setup
+    require "linguist/linguist"
+  end
+
+  def test_is_test_positive
+    assert Linguist::Rust.is_test?("test/api_test.rb")
+    assert Linguist::Rust.is_test?("tests/unit/foo.py")
+    assert Linguist::Rust.is_test?("src/tests/helper.rs")
+    assert Linguist::Rust.is_test?("snapshots/foo.snap")
+  end
+
+  def test_is_test_negative
+    refute Linguist::Rust.is_test?("testfile.rb")
+    refute Linguist::Rust.is_test?("src/main.rs")
+    refute Linguist::Rust.is_test?("lib/foo.rb")
+  end
+
+  def test_is_documentation_positive
+    assert Linguist::Rust.is_documentation?("README")
+    assert Linguist::Rust.is_documentation?("README.md")
+    assert Linguist::Rust.is_documentation?("CHANGELOG.md")
+  end
+
+  def test_is_documentation_negative
+    refute Linguist::Rust.is_documentation?("foo.rb")
+    refute Linguist::Rust.is_documentation?("src/main.rs")
+  end
+
+  def test_is_dependency_management_positive
+    assert Linguist::Rust.is_dependency_management?("Gemfile")
+    assert Linguist::Rust.is_dependency_management?("package.json")
+    assert Linguist::Rust.is_dependency_management?("Cargo.toml")
+    assert Linguist::Rust.is_dependency_management?("requirements.txt")
+    assert Linguist::Rust.is_dependency_management?("go.mod")
+  end
+
+  def test_is_dependency_management_negative
+    refute Linguist::Rust.is_dependency_management?("main.rs")
+    refute Linguist::Rust.is_dependency_management?("lib.rs")
+    refute Linguist::Rust.is_dependency_management?("README.md")
+  end
+end
